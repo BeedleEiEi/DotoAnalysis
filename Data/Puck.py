007896d794046d5b8de2cbe18e2b93942ed148dc
BeedleEiEi <58070023@kmitl.ac.th>
2016-12-03 00:01:36 +0700

    Puck Data

---
+++ Data/Puck.py
@@ -0,0 +1,356 @@
+data = \
+  """
+	Abaddon	-0.65%
+	37.01%
+	64,780
+	Alchemist	-1.44%
+	43.35%
+	128,976
+	Ancient Apparition	0.83%
+	44.64%
+	60,081
+	Anti-Mage	-1.07%
+	45.42%
+	230,976
+	Arc Warden	-0.76%
+	51.21%
+	27,975
+	Axe	0.24%
+	41.68%
+	229,482
+	Bane	0.06%
+	48.52%
+	53,559
+	Batrider	0.20%
+	49.82%
+	38,457
+	Beastmaster	-1.30%
+	45.50%
+	25,884
+	Bloodseeker	0.75%
+	44.18%
+	203,249
+	Bounty Hunter	1.01%
+	48.89%
+	152,939
+	Brewmaster	-0.31%
+	46.30%
+	20,829
+	Bristleback	-0.44%
+	44.80%
+	98,490
+	Broodmother	-0.30%
+	54.80%
+	24,674
+	Centaur Warrunner	-0.75%
+	37.72%
+	76,091
+	Chaos Knight	0.51%
+	44.15%
+	58,046
+	Chen	-1.40%
+	51.23%
+	12,619
+	Clinkz	-0.52%
+	47.67%
+	95,849
+	Clockwerk	-0.43%
+	42.79%
+	57,595
+	Crystal Maiden	1.37%
+	40.01%
+	145,527
+	Dark Seer	-0.74%
+	43.76%
+	39,295
+	Dazzle	0.73%
+	42.76%
+	74,091
+	Death Prophet	-1.23%
+	46.02%
+	50,665
+	Disruptor	-0.23%
+	42.54%
+	73,644
+	Doom	-0.73%
+	49.79%
+	41,210
+	Dragon Knight	-1.50%
+	42.97%
+	63,231
+	Drow Ranger	-0.09%
+	43.20%
+	239,735
+	Earth Spirit	-0.74%
+	51.52%
+	28,477
+	Earthshaker	0.18%
+	44.04%
+	132,544
+	Elder Titan	0.23%
+	39.48%
+	34,434
+	Ember Spirit	-1.50%
+	50.72%
+	71,173
+	Enchantress	-0.41%
+	54.24%
+	35,927
+	Enigma	0.30%
+	42.05%
+	60,616
+	Faceless Void	0.08%
+	46.77%
+	158,379
+	Gyrocopter	-0.51%
+	49.23%
+	43,773
+	Huskar	-1.79%
+	46.11%
+	118,553
+	Invoker	1.91%
+	48.36%
+	355,364
+	Io	-0.15%
+	57.97%
+	20,928
+	Jakiro	-0.31%
+	40.01%
+	57,340
+	Juggernaut	0.37%
+	44.10%
+	300,226
+	Keeper of the Light	0.18%
+	43.95%
+	62,907
+	Kunkka	1.28%
+	47.65%
+	150,728
+	Legion Commander	-0.78%
+	43.99%
+	317,231
+	Leshrac	-1.23%
+	47.35%
+	24,310
+	Lich	1.24%
+	40.27%
+	67,356
+	Lifestealer	-0.53%
+	44.30%
+	142,396
+	Lina	0.17%
+	49.78%
+	92,209
+	Lion	0.17%
+	46.14%
+	168,580
+	Lone Druid	-1.74%
+	52.16%
+	23,344
+	Luna	-0.62%
+	38.33%
+	135,623
+	Lycan	-1.92%
+	45.67%
+	29,318
+	Magnus	0.28%
+	47.06%
+	93,904
+	Medusa	-1.38%
+	40.66%
+	81,990
+	Meepo	0.06%
+	46.34%
+	68,701
+	Mirana	1.33%
+	43.29%
+	282,507
+	Morphling	1.75%
+	47.94%
+	134,317
+	Naga Siren	0.28%
+	48.67%
+	30,461
+	Nature's Prophet	-1.33%
+	51.42%
+	93,923
+	Necrophos	0.47%
+	39.54%
+	135,041
+	Night Stalker	-0.56%
+	43.50%
+	57,897
+	Nyx Assassin	0.18%
+	43.92%
+	110,051
+	Ogre Magi	0.96%
+	40.32%
+	138,744
+	Omniknight	1.03%
+	34.96%
+	134,363
+	Oracle	-0.07%
+	52.41%
+	33,539
+	Outworld Devourer	2.17%
+	46.35%
+	125,109
+	Phantom Assassin	1.04%
+	42.85%
+	441,132
+	Phantom Lancer	2.36%
+	50.97%
+	70,407
+	Phoenix	0.24%
+	42.76%
+	44,881
+	Pudge	0.05%
+	41.36%
+	480,965
+	Pugna	-0.84%
+	46.75%
+	47,857
+	Queen of Pain	1.26%
+	53.25%
+	84,011
+	Razor	-1.60%
+	47.05%
+	69,989
+	Riki	-0.99%
+	38.88%
+	234,287
+	Rubick	-0.28%
+	47.41%
+	123,493
+	Sand King	0.88%
+	45.12%
+	146,253
+	Shadow Demon	0.88%
+	49.27%
+	46,393
+	Shadow Fiend	-0.50%
+	45.60%
+	149,407
+	Shadow Shaman	-0.41%
+	40.68%
+	85,880
+	Silencer	-0.41%
+	39.73%
+	124,800
+	Skywrath Mage	0.69%
+	48.08%
+	65,019
+	Slardar	1.04%
+	43.44%
+	145,056
+	Slark	2.36%
+	44.92%
+	305,250
+	Sniper	-1.33%
+	40.99%
+	306,186
+	Spectre	-1.61%
+	37.21%
+	105,042
+	Spirit Breaker	0.49%
+	41.85%
+	164,839
+	Storm Spirit	-0.05%
+	44.43%
+	176,983
+	Sven	-0.70%
+	38.80%
+	221,535
+	Techies	-0.04%
+	48.20%
+	48,921
+	Templar Assassin	-2.24%
+	46.42%
+	70,357
+	Terrorblade	0.45%
+	45.16%
+	45,353
+	Tidehunter	-0.09%
+	43.55%
+	73,091
+	Timbersaw	0.66%
+	45.47%
+	149,327
+	Tinker	0.77%
+	46.92%
+	175,954
+	Tiny	-0.98%
+	47.94%
+	98,585
+	Treant Protector	-0.55%
+	39.58%
+	40,289
+	Troll Warlord	-0.62%
+	43.59%
+	87,992
+	Tusk	-0.16%
+	51.80%
+	50,798
+	Underlord	-0.72%
+	38.69%
+	50,752
+	Undying	-0.74%
+	42.28%
+	46,605
+	Ursa	-0.50%
+	39.04%
+	134,335
+	Vengeful Spirit	0.34%
+	41.86%
+	91,340
+	Venomancer	-0.84%
+	41.28%
+	70,513
+	Viper	-0.90%
+	44.22%
+	84,087
+	Visage	-1.06%
+	49.62%
+	12,317
+	Warlock	-0.08%
+	37.96%
+	58,967
+	Weaver	0.95%
+	45.52%
+	107,040
+	Windranger	0.31%
+	48.97%
+	141,363
+	Winter Wyvern	0.58%
+	48.64%
+	38,793
+	Witch Doctor	0.65%
+	44.62%
+	87,618
+	Wraith King	-2.03%
+	36.06%
+	162,242
+	Zeus	-0.76%
+	38.63%
+	125,529
+
+"""
+data = data.strip().split()
+hero_name = []
+statistic = []
+value = []
+name = ""
+for run_data in data:
+    if run_data[0] not in "0123456789-+,.%":
+        name += run_data
+    else:
+        value.append(run_data)
+        if name != "":
+            hero_name.append(name)
+        name = ""
+count = 0
+for i in range(len(hero_name)):
+    statistic.append([hero_name[i], str(value[count][:-1]), str(value[count+1][:-1]), value[count+2]])
+    count += 3
+
+print(statistic)
