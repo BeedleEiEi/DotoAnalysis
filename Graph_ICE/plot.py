a2e49033eb00a77b5211d9e39b28db4d7a996d11
BeedleEiEi <58070023@kmitl.ac.th>
2016-12-04 15:09:47 +0700

    Refactoring Code

--- Graph_ICE/plot.py
+++ Graph_ICE/plot.py
@@ -7,11 +7,7 @@ my_data_matchs = file.sheet_by_name('matchs')
 
 def collect_data(number):
     """Collects data from heroes A-Z"""
-    col, row = 0, 0
-    hero_advantage = []
-    hero_name = []
-    hero_winrate = []
-    hero_match = []
+    hero_advantage, hero_name, hero_winrate, hero_match = [], [], [], []
     #number = 1 is abaddon 112 is Zeus
     for i in range(1,113): #Data abaddon start with 1,0 1-13 character
         hero_advantage.append(my_data_advantage.cell(number,i).value)
@@ -20,38 +16,29 @@ def collect_data(number):
         hero_match.append(my_data_matchs.cell(number,i).value)
     return hero_advantage, hero_name, hero_winrate, hero_match
 
-def plot():
-    """Plot Graph by pylab"""
-    import pylab as plt
-    hero = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,\
-            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,\
-            39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,\
-            57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,\
-            75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,\
-            93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108,\
-            109, 110, 111, 112] #index of hero
-    LABELS, names = collect_data(1)
-    plt.bar(hero, LABELS, align='center')
-    plt.xticks(range(112), names) # (x width, names)
-    plt.gca().set_xlim(0.1, 30)
-    plt.show()
-
-def plot_plotly():
-    """Show Graph 112 Heroes sort by Heroes Name"""
+def plot_comparison_graphs():
+    """Show graphs of 112 Heroes sort by Heroes Name"""
     import plotly.offline as offline
-    from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-    from plotly.graph_objs import Bar, Scatter
-    hero_advantage = []
-    hero_name = []
-    hero_winrate = []
-    hero_match = []
-    for i in range(1, 2):
+    from plotly.offline import download_plotlyjs, plot
+    from plotly.graph_objs import Bar
+    hero_advantage, hero_name, hero_winrate, hero_match = [], [], [], []
+    for i in range(1, 113):
         hero_advantage, hero_name, hero_winrate, hero_match = collect_data(i)
         Advantage = Bar(
             {"x":hero_name,
             "y":hero_advantage,"type":"bar","name":'Advantage'
+            }
+            )
+        WinRate = Bar(
+            {"x":hero_name,
+            "y":hero_winrate,"type":"bar","name":'WinRate'
+            }
+            )
+        MatchPlay = Bar(
+            {"x":hero_name,
+            "y":hero_match,"type":"bar","name":'MatchPlayed'
             })
-        data = [Advantage]
+        data = [Advantage, WinRate, MatchPlay]
         offline.plot({'data':data, 'layout':{'title': hero_name[i-1]+" - Advantage is good for range > 0 Disadvantage otherwise", 'font': dict(size=16)}}, filename=str(hero_name[i-1])+".html", image='png')
 
-plot_plotly()
+plot_comparison_graphs()
